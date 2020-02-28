027be5b453e869cfd0e40135306e50329a5fb09f
Eugene_Dolgushev <eugene.dolgushev@whitetigersoft.ru>
2020-02-28 23:46:26 +0300

    Добавить Utils.transliteration

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -11,7 +11,57 @@ object Utils {
     }
 
     fun transliteration(payload: String, divider: String = " "): String {
-        TODO()
+        val map = fillTranslitMap()
+        var result = ""
+        for (char in payload.trim())
+            result += processChar(char, map)
+
+        return result.replace(" ", divider)
+    }
+
+    private fun processChar(char: Char, map: HashMap<Char, String>): String {
+        val ch = map[char.toLowerCase()] ?: char.toString()
+        return if (char.isLowerCase()) ch
+        else ch.capitalize()
+    }
+
+    private fun fillTranslitMap(): HashMap<Char, String> {
+        val map = hashMapOf<Char, String>()
+        map['а'] = "a"
+        map['б'] = "b"
+        map['в'] = "v"
+        map['г'] = "g"
+        map['д'] = "d"
+        map['е'] = "e"
+        map['ё'] = "e"
+        map['ж'] = "zh"
+        map['з'] = "z"
+        map['и'] = "i"
+        map['й'] = "i"
+        map['к'] = "k"
+        map['л'] = "l"
+        map['м'] = "m"
+        map['н'] = "n"
+        map['о'] = "o"
+        map['п'] = "p"
+        map['р'] = "r"
+        map['с'] = "s"
+        map['т'] = "t"
+        map['у'] = "u"
+        map['ф'] = "f"
+        map['х'] = "h"
+        map['ц'] = "c"
+        map['ч'] = "ch"
+        map['ш'] = "sh"
+        map['щ'] = "sh'"
+        map['ъ'] = ""
+        map['ы'] = "i"
+        map['ь'] = ""
+        map['э'] = "e"
+        map['ю'] = "yu"
+        map['я'] = "ya"
+
+        return map
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
