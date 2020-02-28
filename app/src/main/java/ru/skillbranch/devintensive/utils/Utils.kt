7581f7705ffbc7bd409dd9b45802a179731c5679
Eugene_Dolgushev <eugene.dolgushev@whitetigersoft.ru>
2020-02-28 21:18:07 +0300

    Исправить parseFullName

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -3,10 +3,9 @@ package ru.skillbranch.devintensive.utils
 object Utils {
 
     fun parseFullName(fullName: String?): Pair<String?, String?> {
-        val parts: List<String>? = fullName?.split(" ")
-
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
+        val parts = fullName?.trim()?.split(" ")
+        val firstName = parts?.getOrNull(0)?.ifEmpty { null }
+        val lastName = parts?.getOrNull(1)?.ifEmpty { null }
 
         return firstName to lastName
     }
